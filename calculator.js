f07c3ef908b697269701ccd8db554fa33005418c
pavanajayan <jayanpavana0@gmail.com>
2025-06-12 11:31:47 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -16,7 +16,53 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    
+    const clean = expression.replace(/\s+/g, '');
+    if (/[^0-9+\-*/().]/.test(clean)) {
+      throw new Error("Invalid characters in expression");
+    }
+
+    try {
+      
+      this.result = new Function('return ' + clean)();
+    } catch {
+      throw new Error("Invalid expression");
+    }
+  }
+}
 
 module.exports = Calculator;
 
