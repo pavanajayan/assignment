eecbfdde400fe68cbb932591b4de3b557541428a
pavanajayan <jayanpavana0@gmail.com>
2025-06-12 11:29:34 +0530

    Update findLargestElement.js

--- findLargestElement.js
+++ findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
-}
+  let max = arr[0]; 
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
 
+  return max;
+}
 module.exports = findLargestElement;
