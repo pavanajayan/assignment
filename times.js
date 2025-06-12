356f549d7948a52758cd8cc3aa9263709f7dbce1
pavanajayan <jayanpavana0@gmail.com>
2025-06-12 11:34:35 +0530

    Update times.js

--- times.js
+++ times.js
@@ -8,6 +8,17 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateSumTime(n) {
+  let start = new Date(); 
+
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  let end = new Date(); 
+  let timeTaken = (end - start) / 1000; 
+
+  console.log("Sum:", sum);
+  console.log("Time taken (seconds):", timeTaken);
 }
