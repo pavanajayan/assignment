90514d4b6fed69854e25b745b55c1dcc04b22573
pavanajayan <jayanpavana0@gmail.com>
2025-06-12 11:33:44 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -4,7 +4,13 @@
 */
 
 function isPalindrome(str) {
-  return true;
-}
+  str = str.toLowerCase(); 
+  let reversed = "";
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
+}
 module.exports = isPalindrome;
