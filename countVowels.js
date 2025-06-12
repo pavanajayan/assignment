0f5536513a059d8d4e043a92fb2c400181744d26
pavanajayan <jayanpavana0@gmail.com>
2025-06-12 11:32:49 +0530

    Update countVowels.js

--- countVowels.js
+++ countVowels.js
@@ -5,8 +5,18 @@
   Once you've implemented the logic, test your code by running
 */
 
-function countVowels(str) {
-    // Your code here
-}
 
+function countVowels(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let ch = str[i].toLowerCase();
+
+    if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+      count++;
+    }
+  }
+
+  return count;
+}
 module.exports = countVowels;
